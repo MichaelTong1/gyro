2a2d7a2e2eb02fd62aa5c2d657f1079ae85f22bc
Michael Tong <michael_tong@outlook.com>
2018-07-15 12:10:40 -0700

    Added code to sense the x,y, and z coordinates of my iPhone, updated at 0.2 seconds.

--- gyro/ViewController.swift
+++ gyro/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        // Choose how long it takes to update the gyro
+        motionManager.gyroUpdateInterval = 0.2
+        motionManager.startGyroUpdates(to: OperationQueue.current!) { (data, error) in
+            if let myData = data {
+                print (myData.rotationRate)
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
