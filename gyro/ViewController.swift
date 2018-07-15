d58dda4a07ce133763e2a9fb1dcf097ed6f2a64e
Michael Tong <michael_tong@outlook.com>
2018-07-15 11:22:21 -0700

    Initial Commit

---
+++ gyro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gyro
+//
+//  Created by Michael Tong on 7/15/18.
+//  Copyright © 2018 Michael Tong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
